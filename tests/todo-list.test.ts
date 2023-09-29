6a74812adf82f699466b7613d9450cb3ce0326b4
kenitsa <roskenitsa@gmail.com>
2023-09-29 03:02:26 +0300

    Changed loop logic

--- tests/todo-list.test.ts
+++ tests/todo-list.test.ts
@@ -2,13 +2,14 @@ import { describe, test } from '@jest/globals';
 
 import { GenericAssertions } from '../src/generic-assertions';
 import { TodoListApiHelper } from '../src/helper';
+const numberOfTodos: number = 20;
 
 describe('Json Placeholder > Todos', () => {
-  for (let index = 1; index <= 20; index++) {
+  Array.from({ length: numberOfTodos }, (_, index) => index + 1).forEach((index) => {
     test(`${index} todo should have title longer than 20 characters`, async () => {
       const response = await TodoListApiHelper.getTodoByIndex(index);
       GenericAssertions.toHaveHttpStatus(200, response.status);
       GenericAssertions.toHaveCorrectTitleLength(response.data, index);
     });
-  }
+  });
 });
